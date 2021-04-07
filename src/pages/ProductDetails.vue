9700eb5ca8223d9ca5b33e23a162d591f8e8edc1
markoboras0712 <mboras1@etfos.hr>
2021-04-07 11:43:47 +0300

    initial commit

---
+++ src/pages/ProductDetails.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <h2>{{ title }}</h2>
+    <h3>${{ price }}</h3>
+    <p>{{ description }}</p>
+  </section>
+</template>
+
+<script>
+import { computed,inject } from 'vue';
+
+
+export default {
+  props : ['pid'],
+  setup(props) {
+
+    const products = inject ('products');
+    const selectedProduct = computed(() => products.value.find(product => product.id === props.pid)
+    );
+    const title = computed(()=> selectedProduct.value.title );
+    const price = computed(()=> selectedProduct.value.price );
+    const description = computed(()=> selectedProduct.value.description );
+
+
+
+    return { title, price, description };
+  },
+  
+};
+</script>
+
+<style scoped>
+section {
+  margin: 3rem auto;
+  max-width: 40rem;
+  padding: 1rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+</style>
